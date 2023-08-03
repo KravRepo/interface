88e44869f73b59e5c6d0802167b075f68617b6f3
GreatOOne <david@krav.trade>
2023-08-03 22:13:17 +0800

    update style

--- src/components/Home/style.ts
+++ src/components/Home/style.ts
@@ -92,6 +92,31 @@ export const dashboard = css`
     font-size: 14px;
     color: #fff;
   }
+  .my-order {
+    .position-layout {
+      align-items: center;
+      display: grid;
+      padding: 12px 24px 0px;
+      grid-template-columns: 140px minmax(140px, 1fr) 1fr 1fr 1fr 1fr 1fr 0.5fr;
+      font-size: 12px;
+    }
+    .order-layout {
+      align-items: center;
+      display: grid;
+      padding: 12px 24px 0px;
+      grid-template-columns: 80px minmax(140px, 2fr) 1fr 1fr 1fr 1fr 1fr;
+      font-size: 12px;
+    }
+    .no-data {
+      padding-top: 103px;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      font-size: 16px;
+      font-weight: 500;
+      color: #757575;
+    }
+  }
   .income {
     border-radius: 8px;
     padding: 32px;
