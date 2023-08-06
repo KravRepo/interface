d764552e509e38ae6a605e6ff9bbbeb866b6a8a4
GreatOOne <david@krav.trade>
2023-08-06 13:18:29 +0800

    update coming soon

--- src/pages/Statistics.tsx
+++ src/pages/Statistics.tsx
@@ -1,5 +1,37 @@
-import { ComingSoon } from '../components/Home/ComingSoon'
+/** @jsxImportSource @emotion/react */
+import { comingSoon } from '../components/Home/style'
+import { ReactComponent as Coming } from '../assets/imgs/coming_soon.svg'
+import { css } from '@emotion/react'
 
 export const Statistics = () => {
-  return <ComingSoon title={''} />
+  return (
+    <div css={comingSoon}>
+      <p
+        css={css`
+          margin-bottom: 16px;
+        `}
+        className="title"
+      >
+        Statistics
+      </p>
+      <p
+        css={css`
+          margin-left: 40px;
+        `}
+      >
+        Stay tuned! This feature will be launched soon.
+      </p>
+      <div>
+        <Coming className="coming" />
+        <Coming className="coming" />
+        <Coming className="coming" />
+        <Coming className="coming" />
+        <Coming className="coming" />
+        <Coming className="coming" />
+        <Coming className="coming" />
+        <Coming className="coming" />
+        <Coming className="coming" />
+      </div>
+    </div>
+  )
 }
