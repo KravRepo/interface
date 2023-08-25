05f85a19f8ddffcf9c5d0368c04f6d90bd473ca3
GreatOOne <david@krav.trade>
2023-08-25 10:21:56 +0800

    update position layout

--- src/components/Trades/TradeLeft/style.ts
+++ src/components/Trades/TradeLeft/style.ts
@@ -9,7 +9,7 @@ export const myTrade = css`
     align-items: center;
     display: grid;
     padding: 12px 24px 0px;
-    grid-template-columns: 140px minmax(140px, 1fr) 1fr 1fr 1fr 1fr 1fr 0.5fr;
+    grid-template-columns: 120px minmax(200px, 1fr) 1fr 1fr 1fr 1fr 1fr 0.5fr;
     font-size: 12px;
   }
   .order-layout {
