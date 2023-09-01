9145f7ddfca7a8376ec46686d3294562a2eb18a4
GreatOOne <david@krav.trade>
2023-09-01 18:24:01 +0800

    update api url

--- src/constant/chain.ts
+++ src/constant/chain.ts
@@ -17,26 +17,24 @@ export const LINK_ADDRESS = '0x514910771AF9Ca656af840dff83E8264EcF986CA'
 
 export const NODE_ADDRESS = ['0xcf9c4337dcBFd7cBB71c2c5fb1b9e86edEcfb7C8']
 
-export const K_LINE_API = 'https://base-api.krav.trade/krav/v1/klines?symbol=BTCUSDT&interval=1d'
+export const K_LINE_API = 'https://eth-api.krav.trade/krav/v1/klines?symbol=BTCUSDT&interval=1d'
 
-export const QUANTO_API = 'https://base-api.krav.trade/krav/v1/list/quanto'
+export const QUANTO_API = 'https://eth-api.krav.trade/krav/v1/list/quanto'
 
-export const TRADE_HISTORY_API = 'https://base-api.krav.trade/krav/v1/list/market'
+export const TRADE_HISTORY_API = 'https://eth-api.krav.trade/krav/v1/list/market'
 
-export const DASHBOARD_OVERVIEW_API = 'https://base-api.krav.trade/krav/v1/overview'
-
-export const MARKET_CHANGE_API = 'https://base-api.krav.trade/krav/v1/price/24hr?symbol=BTCUSDT'
+export const DASHBOARD_OVERVIEW_API = 'https://eth-api.krav.trade/krav/v1/overview'
 
 export const TEST_CHAIN_ID = ChainId.MAINNET
 
 export const BTC_CONTRACT = '0x1b44F3514812d835EB1BDB0acB33d3fA3351Ee43'
 
-export const BTC_PRICE_API = 'https://base-api.krav.trade/krav/v1/price?symbol=BTCUSDT'
+export const BTC_PRICE_API = 'https://eth-api.krav.trade/krav/v1/price?symbol=BTCUSDT'
 
 export const KRAV_STAKE = '0xe480d334e6BF7693b12982e9Bf116F3BEeD386a0'
 
 export const KRAV_ADDRESS = '0x8b99c4DE6f3D396a9d4BBcAF9fA138F5393299FE'
 
-export const DashBoard_USER_OVERVIEW_API = 'https://base-api.krav.trade/krav/v1/user/asset/amount?account='
+export const DashBoard_USER_OVERVIEW_API = 'https://eth-api.krav.trade/krav/v1/user/asset/amount?account='
 
 export const BASE_KRAV_TRADING_ADDRESS = '0x8975Fdbad4884998AC36669d126471cE239D94b1'
