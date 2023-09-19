f707686afd32034ba1e9ec2d180096af359e2fef
GreatOOne <david@krav.trade>
2023-09-19 17:22:57 +0800

    Replace the second chart with trading view

--- src/components/Trades/TradeLeft/SecondChart.tsx
+++ src/components/Trades/TradeLeft/SecondChart.tsx
@@ -1,172 +1,51 @@
 /** @jsxImportSource @emotion/react */
-import { css, useTheme } from '@mui/material'
-import { useCallback, useEffect, useMemo, useRef, useState } from 'react'
-import * as echarts from 'echarts'
+import { Box, useTheme } from '@mui/material'
+import { useCallback, useEffect } from 'react'
+import { css } from '@emotion/react'
 
 export const SecondChart = () => {
   const theme = useTheme()
-  const [secondCharts, setSecondCharts] = useState<null | echarts.ECharts>(null)
-  const [chartData, setChartData] = useState([] as any[])
-  const secondChartRef = useRef(null)
-  const SecondChartOptions = useMemo(() => {
-    return {
-      backgroundColor: theme.background.primary,
-      tooltip: {
-        trigger: 'axis',
-        axisPointer: {
-          animation: true,
-        },
-      },
-      grid: {
-        left: '18px',
-        right: '16px',
-        top: '26px',
-        bottom: '16px',
-        containLabel: true,
-      },
-      xAxis: {
-        type: 'time',
-        splitLine: {
-          show: false,
-        },
-      },
-      yAxis: {
-        min: function (value: any) {
-          return value.min - 1
-        },
-        max: function (value: any) {
-          return value.max + 1
-        },
-        type: 'value',
-        boundaryGap: [0, '100%'],
-        splitLine: {
-          show: false,
-        },
-        position: 'right',
-      },
-      series: [
-        {
-          name: 'BTC Price',
-          type: 'line',
-          showSymbol: false,
-          data: chartData,
-          smooth: true,
-          endLabel: {
-            show: true,
-            backgroundColor: 'red',
-            padding: [4, 8, 4, 8],
-            color: '#fff',
-            borderRadius: 2,
-          },
-          lineStyle: {
-            color: '#DB4C40',
-          },
-          areaStyle: {
-            color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
-              {
-                offset: 0,
-                color: 'rgba(219, 149, 64, 0.50)',
-              },
-              {
-                offset: 1,
-                color: 'rgba(219, 76, 64, 0.00)',
-              },
-            ]),
-          },
-        },
-      ],
-    }
-  }, [theme])
-
-  const updateCharts = useCallback(
-    (data: number) => {
-      if (secondCharts) {
-        const newData = chartData
-        const nowStr = new Date().toLocaleString()
-        newData.push([nowStr, data])
-        setChartData(newData)
-        secondCharts.setOption({
-          series: [
-            {
-              data: newData,
-              areaStyle: {
-                color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
-                  {
-                    offset: 0,
-                    color: 'rgba(219, 149, 64, 0.50)',
-                  },
-                  {
-                    offset: 1,
-                    color: 'rgba(219, 76, 64, 0.00)',
-                  },
-                ]),
-              },
-            },
-          ],
-        })
-      }
-    },
-    [secondCharts]
-  )
-
-  const connectWs = useCallback(
-    (url: string) => {
-      setTimeout(() => {
-        const ws1D = new WebSocket(url)
-        ws1D.onmessage = function (msg) {
-          if (msg.data) {
-            new Response(msg.data).json().then((res) => {
-              updateCharts(Number(res.k.c))
-            })
-          }
-        }
-      }, 2000)
-    },
-    [updateCharts]
-  )
-
-  useEffect(() => {
-    const ws1D = new WebSocket('wss://wss.krav.trade:8800/')
-    ws1D.onmessage = function (msg) {
-      if (msg.data && secondCharts) {
-        new Response(msg.data).json().then((res) => {
-          updateCharts(Number(res.k.c))
-        })
-      }
-    }
-    ws1D.onclose = function () {
-      setTimeout(() => connectWs('wss://wss.krav.trade:8800/'), 2000)
-    }
-  }, [secondCharts])
-
-  useEffect(() => {
-    if (secondChartRef.current) {
-      if (secondCharts === null) {
-        const charts = echarts.init(secondChartRef.current, undefined, { renderer: 'svg' })
-        charts.setOption(SecondChartOptions)
-        setSecondCharts(charts)
-        window.addEventListener('resize', () => {
-          charts?.resize()
-        })
-      }
-      secondCharts?.setOption(SecondChartOptions)
-      window.addEventListener('resize', () => {
-        secondCharts?.resize()
+  const createWidget = useCallback(() => {
+    if (document.getElementById('tradingview_2daf6') && 'TradingView' in window) {
+      new window.TradingView.widget({
+        autosize: true,
+        symbol: 'BINANCE:BTCUSDT',
+        interval: '1',
+        timezone: 'Etc/UTC',
+        theme: theme.palette.mode === 'dark' ? 'dark' : 'light',
+        style: '2',
+        locale: 'en',
+        enable_publishing: false,
+        hide_top_toolbar: true,
+        hide_legend: true,
+        save_image: false,
+        hide_volume: true,
+        container_id: 'tradingview_2daf6',
       })
     }
-    return () => {
-      secondCharts?.dispose()
-      setSecondCharts(null)
-    }
-  }, [secondChartRef.current, theme])
+  }, [theme])
+  useEffect(() => {
+    new Promise((resolve) => {
+      const script = document.createElement('script')
+      script.id = 'tradingview-widget-loading-script'
+      script.src = 'https://s3.tradingview.com/tv.js'
+      script.type = 'text/javascript'
+      script.onload = resolve
+      document.head.appendChild(script)
+    }).then(() => createWidget())
+  }, [createWidget])
   return (
-    <div
-      css={css`
-        height: 481px;
-        border-radius: 8px;
-        overflow: hidden;
-      `}
-      ref={secondChartRef}
-    />
+    <Box
+      sx={{ mt: '12px', borderRadius: '8px', height: '481px', padding: '18px', background: theme.background.primary }}
+    >
+      <div className="tradingview-widget-container">
+        <div
+          css={css`
+            height: 440px;
+          `}
+          id="tradingview_2daf6"
+        />
+      </div>
+    </Box>
   )
 }
