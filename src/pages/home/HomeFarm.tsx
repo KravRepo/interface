23136f2f17f0cef532a5d1b088cfb5bb254e64d6
GreatOOne <david@krav.trade>
2023-09-22 17:07:45 +0800

    merge and fix config

--- src/pages/home/HomeFarm.tsx
+++ src/pages/home/HomeFarm.tsx
@@ -1,11 +1,13 @@
 /** @jsxImportSource @emotion/react */
-import { NewFarm } from '../../components/Home/NewFarm'
+// import { NewFarm } from '../../components/Home/NewFarm'
 import { HomeLayout } from './HomeLayout'
+import { Farm } from '../../components/Home/Farm'
 
 export const HomeFarm = () => {
   return (
     <HomeLayout>
-      <NewFarm />
+      <Farm />
+      {/*<NewFarm />*/}
     </HomeLayout>
   )
 }
