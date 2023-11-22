4a8ae5334ccb840c98da12fc858c498541c01d04
GreatOOne <david@krav.trade>
2023-11-22 11:28:44 +0800

    update referral

--- src/pages/home/HomeReferral.tsx
+++ src/pages/home/HomeReferral.tsx
@@ -1,19 +1,11 @@
 /** @jsxImportSource @emotion/react */
-// import { Referral } from '../../components/Home/Referral'
 import { HomeLayout } from './HomeLayout'
-import { ComingSoon } from '../../components/Home/ComingSoon'
 import { Referral } from '../../components/Home/Referral'
-import { useRootStore } from '../../store/root'
-import { ChainId } from '../../constant/chain'
 
 export const HomeReferral = () => {
-  const expectChainId = useRootStore((store) => store.expectChainId)
   return (
     <HomeLayout>
-      <>
-        {expectChainId !== ChainId.BASE && expectChainId !== ChainId.BASE_TEST && <ComingSoon title={'Referral'} />}
-        {(expectChainId === ChainId.BASE || expectChainId === ChainId.BASE_TEST) && <Referral />}
-      </>
+      <Referral />
     </HomeLayout>
   )
 }
