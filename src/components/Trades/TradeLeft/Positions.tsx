1fb9fd18b290c42aeb3aee7c3e7081bbe94409cb
GreatOOne <david@krav.trade>
2023-11-23 16:43:59 +0800

    update connect wallet button

--- src/components/Trades/TradeLeft/Positions.tsx
+++ src/components/Trades/TradeLeft/Positions.tsx
@@ -4,9 +4,11 @@ import { useRootStore } from '../../../store/root'
 import { PositionsItem } from './PositionsItem'
 import { useTheme } from '@mui/material'
 import { useWeb3React } from '@web3-react/core'
+import KRAVButton from '../../KravUIKit/KravButton'
 
 export const Positions = () => {
   const userOpenTradeList = useRootStore((state) => state.userOpenTradeList)
+  const setWalletDialogVisibility = useRootStore((state) => state.setWalletDialogVisibility)
   const { account } = useWeb3React()
   const theme = useTheme()
   return (
@@ -29,7 +31,16 @@ export const Positions = () => {
         <span>Close</span>
       </div>
       {userOpenTradeList.length === 0 && account && <div className="no-data">No open position</div>}
-      {!account && <div className="no-data">Connect wallet</div>}
+      {!account && (
+        <div className="no-data">
+          <KRAVButton
+            onClick={() => setWalletDialogVisibility(true)}
+            sx={{ width: '113px', mt: '32px', mb: '25px', zIndex: 3 }}
+          >
+            Connect Wallet
+          </KRAVButton>
+        </div>
+      )}
       {userOpenTradeList.length > 0 &&
         account &&
         userOpenTradeList.map((openTrade, index) => {
