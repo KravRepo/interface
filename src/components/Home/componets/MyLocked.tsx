8567db8d9607488d329245a94d80b807e0437559
GreatOOne <david@krav.trade>
2023-11-24 10:51:04 +0800

    show stake booster

--- src/components/Home/componets/MyLocked.tsx
+++ src/components/Home/componets/MyLocked.tsx
@@ -2,7 +2,7 @@
 import { ReactComponent as ToVoteIcon } from '../../../assets/imgs/to_vote.svg'
 import KRAVButton from '../../KravUIKit/KravButton'
 import KravButtonHollow from '../../KravUIKit/KravButtonHollow'
-import { css, Link, useTheme } from '@mui/material'
+import { css, Link, Tooltip, useTheme } from '@mui/material'
 import { UserLockPosition } from '../../../hook/hookV8/useGetUserKravLock'
 import { formatNumber, getBigNumberStr } from '../../../utils'
 import moment from 'moment'
@@ -10,6 +10,9 @@ import { FeesRewardList } from '../../../hook/hookV8/useGetClaimableTokensFee'
 import { useMemo } from 'react'
 import { useClaimFeesReward } from '../../../hook/hookV8/useClaimFeesReward'
 import BigNumber from 'bignumber.js'
+import { align } from '../../../globalStyle'
+import { ReactComponent as QuestionIcon } from '../../../assets/imgs/question.svg'
+import { ReactComponent as BoostIcon } from '../../../assets/imgs/boost_icon.svg'
 
 type MyLockedProp = {
   userLockPosition: UserLockPosition
@@ -62,51 +65,50 @@ export const MyLocked = ({ userLockPosition, userFeesRewardList, LpBooster, trad
                 .format('MMM DD, YYYY HH:mm A')}
           &nbsp;UTC
         </span>
-        {/*<span>Sep 21, 2021 08:30 AM UTC </span>*/}
       </div>
-      {/*<div*/}
-      {/*  css={css`*/}
-      {/*    display: flex;*/}
-      {/*    align-items: center;*/}
-      {/*    justify-content: space-between;*/}
-      {/*    margin-bottom: 12px;*/}
-      {/*  `}*/}
-      {/*>*/}
-      {/*  <Tooltip*/}
-      {/*    title={*/}
-      {/*      'Locking KRAV can get krav income and veKRAV rights and interests, which can Boost your yield up to 2.5x'*/}
-      {/*    }*/}
-      {/*  >*/}
-      {/*    <div css={align}>*/}
-      {/*      <BoostIcon />*/}
-      {/*      <span>&nbsp;My current trade boost :&nbsp;</span>*/}
-      {/*      <QuestionIcon />*/}
-      {/*    </div>*/}
-      {/*  </Tooltip>*/}
+      <div
+        css={css`
+          display: flex;
+          align-items: center;
+          justify-content: space-between;
+          margin-bottom: 12px;
+        `}
+      >
+        <Tooltip
+          title={
+            'Locking KRAV can get krav income and veKRAV rights and interests, which can Boost your yield up to 2.5x'
+          }
+        >
+          <div css={align}>
+            <BoostIcon />
+            <span>&nbsp;My current trade boost :&nbsp;</span>
+            <QuestionIcon />
+          </div>
+        </Tooltip>
 
-      {/*  <div>{getBigNumberStr(tradeBooster, 4)}</div>*/}
-      {/*</div>*/}
-      {/*<div*/}
-      {/*  css={css`*/}
-      {/*    display: flex;*/}
-      {/*    align-items: center;*/}
-      {/*    justify-content: space-between;*/}
-      {/*    margin-bottom: 32px;*/}
-      {/*  `}*/}
-      {/*>*/}
-      {/*  <Tooltip*/}
-      {/*    title={*/}
-      {/*      'Locking KRAV can get krav income and veKRAV rights and interests, which can Boost your yield up to 2.5x'*/}
-      {/*    }*/}
-      {/*  >*/}
-      {/*    <div css={align}>*/}
-      {/*      <BoostIcon />*/}
-      {/*      <span>&nbsp;My current Liquidity Provider boost :&nbsp;</span>*/}
-      {/*      <QuestionIcon />*/}
-      {/*    </div>*/}
-      {/*  </Tooltip>*/}
-      {/*  <div>{getBigNumberStr(LpBooster, 4)}</div>*/}
-      {/*</div>*/}
+        <div>{getBigNumberStr(tradeBooster, 4)}</div>
+      </div>
+      <div
+        css={css`
+          display: flex;
+          align-items: center;
+          justify-content: space-between;
+          margin-bottom: 32px;
+        `}
+      >
+        <Tooltip
+          title={
+            'Locking KRAV can get krav income and veKRAV rights and interests, which can Boost your yield up to 2.5x'
+          }
+        >
+          <div css={align}>
+            <BoostIcon />
+            <span>&nbsp;My current Liquidity Provider boost :&nbsp;</span>
+            <QuestionIcon />
+          </div>
+        </Tooltip>
+        <div>{getBigNumberStr(LpBooster, 4)}</div>
+      </div>
       <KRAVButton disabled={!unlockButtonEnable} sx={{ mb: '32px' }}>
         Unlock
       </KRAVButton>
