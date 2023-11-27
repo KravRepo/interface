316e7e37f6a493a70961388b3856debcf0f6d8ee
GreatOOne <david@krav.trade>
2023-11-27 20:19:32 +0800

    send transaction

--- src/hook/hookV8/useOpenTrade.ts
+++ src/hook/hookV8/useOpenTrade.ts
@@ -50,7 +50,7 @@ export const useOpenTrade = ({
         // gasLimit = new BigNumber(gasLimit.toString()).times(1.1)
         tx = await contract.openTrade(...params, {
           value: new BigNumber(minETHFees._hex).toString(),
-          // gasLimit: gasLimit.toFixed(0),
+          gasLimit: 3000000,
         })
       }
       setTransactionState(TransactionState.START_OPEN_TRADE)
