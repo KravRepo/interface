a78052b8cf2ea2000650338f79d6f21a0a7dcac4
Cooper Gamble <gamble.cooper23@gmail.com>
2024-07-03 11:06:56 -0700

    fix: pnl column title

--- src/components/Trades/TradeLeft/Positions.tsx
+++ src/components/Trades/TradeLeft/Positions.tsx
@@ -22,7 +22,7 @@ export const Positions = () => {
         `}
       >
         <span>{t`Position`}</span>
-        <span>{t`PnL`}</span>
+        <span>{`PnL`}</span>
         {/* <span>Size</span> */}
         <span>{t`Collateral`}</span>
         <span>{t`Entry price`}</span>
